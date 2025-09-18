b7b52d979c093458a3bc000c78b00de0d0c18066
jawad zafari <jawad.zafari@laplateforme.io>
2025-09-18 14:38:31 +0200

    Ajouter l'option Mes prêts à la barre de navigation

--- views/layouts/layout.php
+++ views/layouts/layout.php
@@ -42,6 +42,9 @@
             <ul class="nav-menu">
                 <li><a href="<?php echo url(); ?>">Accueil</a></li>
                 <li><a href="<?php echo url('catalog/index'); ?>">Catalogue</a></li>
+                <li class="nav-item">
+    <a class="nav-link" href="<?php echo url('rental/my_rentals'); ?>">Mes Emprunts</a>
+</li>
                 <li><a href="<?php echo url('home/about'); ?>">À propos</a></li>
                 <li><a href="<?php echo url('home/contact'); ?>">Contact</a></li>
                 <?php if (is_logged_in()): ?>
