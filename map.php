c0d063e7991b7824bd375f958d5ef2399822aa70
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-29 13:04:22 +0200

    Restructuration de la structure des fichiers

--- map.php
+++ map.php
@@ -7,7 +7,7 @@ mediatheque_paris_grp1/
 │   ├──admin_controller.php
 │   ├──auth_controller.php
 │   ├──catalog_controller.php
-│   ├──rental_controller.php
+│   ├──borrow_controller.php
 |   └──home_controller.php
 ├── core/
 │   ├──database.php
@@ -19,23 +19,34 @@ mediatheque_paris_grp1/
 │   └──helpers.php
 ├── models/
 |    ├──catalogue_model.php
-|    ├──loan_model.php
 |    ├──media_model.php
 |    ├──item_model.php
-|    ├──rental_model.php
+|    ├──borrow_model.php
 |    └──user_model.php
 ├── public/
 |    ├──assets/
 |    |   ├──css/
+|    |   |  ├──admin.css    
+|    |   |  ├──auth.css   
+|    |   |  ├──banner.css   
+|    |   |  ├──borrow.css    
+|    |   |  ├──catalog.css   
+|    |   |  ├──header.css
+|    |   |  ├──home.css  
+|    |   |  ├──layout.css  
+|    |   |  ├──style(@media).css  
 |    |   |  └──style.css
 |    |   ├──images/    
 |    |   └──js/
-|    |      └──app.js                                                           
+|    |     ├──borrow.js
+|    |     └──app.js                                                           
 |    ├──.htaccess
 |    └──index.php
 ├── views/
 │   ├── admin/
 |   |   ├──dashboard.php    
+|   |   ├──sidebar.php    
+|   |   ├──loans_edit.php    
 |   |   ├──loans_list.php    
 |   |   ├──media_edit.php    
 |   |   ├──media_list.php    
@@ -44,25 +55,29 @@ mediatheque_paris_grp1/
 │   ├── auth/
 |   |   ├──login.php    
 |   |   └──register.php
+│   ├── borrow/
+|   |   └──my_borrow.php
+|   |   
 │   ├── catalog/
-|   |   ├──books.php   
-|   |   ├──games.php   
-|   |   ├──index.php   
-|   |   └──movies.php
+|   |   ├──details.php   
+|   |   └──index.php   
 |   |  
 |   ├──errors/
 |   |   └──404.php
 |   ├──home/  
 |   |   ├──about.php
-|   |   ├──catalogue.php
+|   |   ├──upload.php
 |   |   ├──contact.php
 |   |   ├──index.php
 |   |   ├──profile.php
 |   |   └──test.php
-|   ├──layouts/  
-|   |  └──layouts.php
-|   └──rental/
-|      └──my_rentals.php
+|   └──layouts/ 
+|      ├──banner.php
+|      ├──search.php
+|      ├──header.php
+|      ├──admin_layout.php
+|      └──layouts.php
+|  
 ├── .gitignore
 ├──bootstrap.php
 ├──CHANGELOG.md
