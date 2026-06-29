81f2a6cf0e57f98e9b34d993dbfd947e4168eee8
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-29 23:24:42 +0200

    Ajouter la fonction catalog_index pour gérer l'affichage du catalogue avec pagination et recherche

--- controllers/catalog_controller.php
+++ controllers/catalog_controller.php
@@ -10,4 +10,27 @@ function catalog_index() {
     $per_page = 20;
     $current_page = isset($_GET['page']) && is_numeric($_GET['page']) ? (int)$_GET['page'] : 1;
     
-   
+    $total_items = get_items_count_by_type($search_type, $search_term, $search_genre, $search_availability);
+    $total_pages = max(1, ceil($total_items / $per_page));
+    $current_page = max(1, min($current_page, $total_pages));
+    $offset = ($current_page - 1) * $per_page;
+    
+    $items = get_items_by_type($search_type, $search_term, $search_genre, $search_availability, $per_page, $offset);
+    
+    $genres_list = get_all_genres();
+
+    $data = [
+        'title' => 'Catalogue',
+        'items' => $items,
+        'current_page' => $current_page,
+        'total_pages' => $total_pages,
+        'search_term' => $search_term,
+        'search_type' => $search_type,
+        'search_genre' => $search_genre,
+        'search_availability' => $search_availability,
+        'genres_list' => $genres_list 
+    ];
+    
+    load_view_with_layout('catalog/index', $data);
+}
+
