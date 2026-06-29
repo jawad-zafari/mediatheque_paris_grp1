f00505360465e51e148ea3c8c7070747421a26c8
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-29 13:27:04 +0200

    Ajouter le fichier banner avec la section de présentation et le sous-titre

---
+++ views/layouts/banner.php
@@ -0,0 +1,9 @@
+<?php
+?>
+<section class="global-banner">
+    <div class="banner-overlay"></div>
+    <div class="hero-banner">
+        <h1>Médiathèque de paris</h1>
+        <p class="hero-subtitle">Découvrez notre vaste collection de livres, films et jeux vidéo.</p>
+    </div>
+</section>
