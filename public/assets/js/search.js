bbafd16798a3da7db2fa99c47a348607611266ad
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-29 13:19:44 +0200

    Ajouter la gestion des éléments de recherche dans le script de recherche

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-   
+    const searchInput = document.getElementById('liveSearchInput');
+    const resultsBox = document.getElementById('autocompleteResults');
+
+    if (!searchInput || !resultsBox) return;
+
+    
 });
