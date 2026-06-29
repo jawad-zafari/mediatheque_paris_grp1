21d625f824d3c389f34ce9be81aca8f256c14ea5
jawad zafari <jawad.zafari@laplateforme.io>
2026-06-29 23:25:56 +0200

    Mettre à jour le contrôleur des pages statiques pour inclure la gestion du profil et supprimer les fonctions inutilisées

--- controllers/home_controller.php
+++ controllers/home_controller.php
@@ -1,8 +1,11 @@
 <?php
 /**
- * Contrôleur pour les pages statiques (À propos, Contact)
+ * Contrôleur pour les pages statiques et la gestion du profil
  */
 
+/* =========================================================
+   PAGES STATIQUES
+========================================================= */
 function home_about() {
     $data = [
         'title' => 'À propos'
@@ -10,22 +13,3 @@ function home_about() {
     load_view_with_layout('home/about', $data);
 }
 
-function home_contact() {
-    $data = [
-        'title' => 'Contact'
-    ];
-    load_view_with_layout('home/contact', $data);
-}
-
-function home_profile() {
-    if (!is_logged_in()) {
-        redirect('auth/login');
-    }
-    
-    $data = [
-        'title' => 'Mon Profil'
-    ];
-    load_view_with_layout('home/profile', $data);
-}
-
-?>
